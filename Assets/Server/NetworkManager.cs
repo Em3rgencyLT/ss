6bca6832413bcb485236d4f6fe53270a1500d72e
Em3rgencyLT <Em3rgency.lt@gmail.com>
2016-04-28 11:12:11 +0300

    begin merge client and server

---
+++ Assets/Server/NetworkManager.cs
@@ -0,0 +1,27 @@
+using System;
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkManager : MonoBehaviour {
+
+	void Start () 
+	{
+		NetworkServer.Listen(1314);
+		Console.WriteLine("Server started.");
+
+        NetworkServer.RegisterHandler(MsgType.Connect, OnConnected);
+        NetworkServer.RegisterHandler(MsgType.Disconnect, OnDisconnected);
+	}
+
+    private void OnConnected (NetworkMessage netMsg)
+    {
+        Console.WriteLine("Client from " + netMsg.conn.address + " connected.");
+        //TODO: store and handle connected player data
+    }
+
+    private void OnDisconnected (NetworkMessage netMsg)
+    {
+        Console.WriteLine(netMsg.conn.address + " disconnected.");
+        //TODO: handle player disconnect
+    }
+}
